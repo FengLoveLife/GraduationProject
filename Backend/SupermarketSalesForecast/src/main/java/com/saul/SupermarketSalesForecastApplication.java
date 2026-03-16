c98814e13dfa4e08675bcacd22dfa7d0c7d818b2
FengLoveLife <lovelifefeng@gmail.com>
2026-03-16 14:35:18 +0800

    fix(backend): 修复 MapperScan 扫描路径
    使用通配符扫描所有模块的 mapper 包：com.saul.**.mapper

--- Backend/SupermarketSalesForecast/src/main/java/com/saul/SupermarketSalesForecastApplication.java
+++ Backend/SupermarketSalesForecast/src/main/java/com/saul/SupermarketSalesForecastApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.saul.mapper")
+@MapperScan("com.saul.**.mapper")
 public class SupermarketSalesForecastApplication {
 
     public static void main(String[] args) {
